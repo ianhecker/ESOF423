a7e46f87cdf9ed57a5ea184d358b7203075478ae
Ian Hecker <ianhecker@yahoo.com>
2019-02-20 22:32:56 -0700

    fixed test-case in Customer testing

--- A1/src/Participation/Customer_test.java
+++ A1/src/Participation/Customer_test.java
@@ -7,24 +7,6 @@ import static org.junit.Assert.* ;
  * the class Customer.
  */
 public class Customer_test {
-
-	/*
-	@Test
-	public void test1() {
-		System.out.println("Provide here a short description of your test purpose here...") ;
-		Customer C = new Customer(0,"Duffy Duck","") ;
-		assertTrue(C.getCostToPay() == 0) ; 
-	}
-	
-	@Test
-	public void test2() { 
-		System.out.println("Provide a short description...") ;
-		// an example of test that fails, in this case trivially:
-		assertTrue(1/0 == 0) ;
-	}
-	
-	// and so on ...
-	 */
 	
 	//Tests for the return value of zero for no customer participations
 	@Test
@@ -105,15 +87,24 @@ public class Customer_test {
 		System.out.println("** Testing Class: Customer | Method: getDiscountValue | Against: One Customer Discount 5 Pack w/ 5 Participations");
 		Customer c = new Customer(2, "test 2.3", "");
 		Discount d = new Discount_5pack();
-		Service s = new Service(2, "Linguini", 100);
-		Participation p = new Participation(c, s);
+		Service s0 = new Service(0, "A", 200);
+		Service s1 = new Service(1, "B", 200);
+		Service s2 = new Service(2, "C", 200);
+		Service s3 = new Service(3, "D", 200);
+		Service s4 = new Service(4, "E", 200);		
+		Participation p0 = new Participation(c, s0);
+		Participation p1 = new Participation(c, s1);
+		Participation p2 = new Participation(c, s2);
+		Participation p3 = new Participation(c, s3);
+		Participation p4 = new Participation(c, s4);
+		
 		
 		c.discounts.add(d);
-		c.participations.add(p);
-		c.participations.add(p);
-		c.participations.add(p);
-		c.participations.add(p);
-		c.participations.add(p);		
+		c.participations.add(p0);
+		c.participations.add(p1);
+		c.participations.add(p2);
+		c.participations.add(p3);
+		c.participations.add(p4);		
 		assertTrue(c.getDiscountValue() == 10);
 	}
 	
@@ -122,7 +113,8 @@ public class Customer_test {
 	public void gdv5() {
 		System.out.println("** Testing Class: Customer | Method: getDiscountValue | Against: One Customer 2 Discount 5 Packs w/ 5 Participations");
 		Customer c = new Customer(2, "test 2.3", "");
-		Discount d = new Discount_5pack();
+		Discount d0 = new Discount_5pack();
+		Discount d1 = new Discount_5pack();
 		Service s0 = new Service(0, "A", 200);
 		Service s1 = new Service(1, "B", 200);
 		Service s2 = new Service(2, "C", 200);
@@ -134,8 +126,8 @@ public class Customer_test {
 		Participation p3 = new Participation(c, s3);
 		Participation p4 = new Participation(c, s4);
 		
-		c.discounts.add(d);
-		c.discounts.add(d);			
+		c.discounts.add(d0);
+		c.discounts.add(d1);			
 		c.participations.add(p0);
 		c.participations.add(p1);
 		c.participations.add(p2);
@@ -207,10 +199,7 @@ public class Customer_test {
 		c.participations.add(p3);
 		c.participations.add(p4);		
 		assertTrue(c.getCostToPay() == 200*5-10);
-	}
-	
-	
-	
+	}	
 	//-----------------------------------------------------------------------------------
 	//Tests for the return value of zero for no customer participation groups
 	@Test
